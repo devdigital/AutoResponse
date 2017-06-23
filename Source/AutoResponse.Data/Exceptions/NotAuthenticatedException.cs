fc849ae901c69f5b0f1eb812967a0e06705de29a
devdigital <devdigital.cloud@gmail.com>
2017-06-23 09:02:29 +0100

    fix(not-authenticated): added message to exception

--- Source/AutoResponse.Data/Exceptions/NotAuthenticatedException.cs
+++ Source/AutoResponse.Data/Exceptions/NotAuthenticatedException.cs
@@ -4,5 +4,9 @@
 
     public class NotAuthenticatedException : Exception
     {
+        public NotAuthenticatedException(string message)
+            : base(message)
+        {            
+        }
     }
 }
