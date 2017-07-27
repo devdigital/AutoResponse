6054f993d09aafd9daec190b675433cd3915eef0
devdigital <devdigital.cloud@gmail.com>
2017-07-27 14:59:57 +0100

    fix(comments): removed commented code

--- Source/AutoResponse.Core/ApiEvents/AutoResponseApiEvent.cs
+++ Source/AutoResponse.Core/ApiEvents/AutoResponseApiEvent.cs
@@ -1,19 +1,6 @@
 namespace AutoResponse.Core.ApiEvents
 {
-    using System;
-
     public class AutoResponseApiEvent
     {
-        //public AutoResponseApiEvent(string message)
-        //{
-        //    if (string.IsNullOrWhiteSpace(message))
-        //    {
-        //        throw new ArgumentNullException(nameof(message));
-        //    }
-
-        //    this.Message = message;
-        //}
-
-        //public string Message { get; }
     }
 }
