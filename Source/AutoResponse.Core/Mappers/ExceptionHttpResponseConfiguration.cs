8916d47c91a2d31947b8eb0ee48d2bcf3fa14d20
devdigital <devdigital.cloud@gmail.com>
2017-08-24 13:52:18 +0100

    feat(update-mapping): adds update and remove mapping methods

--- Source/AutoResponse.Core/Mappers/ExceptionHttpResponseConfiguration.cs
+++ Source/AutoResponse.Core/Mappers/ExceptionHttpResponseConfiguration.cs
@@ -7,35 +7,63 @@ namespace AutoResponse.Core.Mappers
 
     public class ExceptionHttpResponseConfiguration
     {
-        private readonly IDictionary<Type, Func<ExceptionHttpResponseContext, object, IHttpResponse>> mappers;
+        private readonly IDictionary<Type, Func<ExceptionHttpResponseContext, object, IHttpResponse>> mappings;
         
-        public ExceptionHttpResponseConfiguration(IDictionary<Type, Func<ExceptionHttpResponseContext, object, IHttpResponse>> mappers)
+        public ExceptionHttpResponseConfiguration(IDictionary<Type, Func<ExceptionHttpResponseContext, object, IHttpResponse>> mappings)
         {   
-            if (mappers == null)
+            if (mappings == null)
             {
-                throw new ArgumentNullException(nameof(mappers));
+                throw new ArgumentNullException(nameof(mappings));
             }
 
-            this.mappers = mappers;
+            this.mappings = mappings;
         }
 
         public void AddMapping<TApiEvent>(
-            Func<ExceptionHttpResponseContext, TApiEvent, IHttpResponse> mapper) where TApiEvent : class
+            Func<ExceptionHttpResponseContext, TApiEvent, IHttpResponse> mapping) where TApiEvent : class
         {
-            if (mapper == null)
+            if (mapping == null)
             {
-                throw new ArgumentNullException(nameof(mapper));
+                throw new ArgumentNullException(nameof(mapping));
             }
 
-            if (this.mappers.ContainsKey(typeof(TApiEvent)))
+            if (this.mappings.ContainsKey(typeof(TApiEvent)))
             {
                 throw new InvalidOperationException(
-                    $"API event type {typeof(TApiEvent).Name} mapper already registered");
+                    $"API event type {typeof(TApiEvent).Name} mapping already registered");
             }
 
-            this.mappers.Add(
+            this.mappings.Add(
                 typeof(TApiEvent),
-                (c, e) => mapper(c, e as TApiEvent));
+                (c, e) => mapping(c, e as TApiEvent));
+        }
+
+        public void UpdateMapping<TApiEvent>(Func<ExceptionHttpResponseContext, TApiEvent, IHttpResponse> mapping)
+            where TApiEvent : class
+        {
+            if (mapping == null)
+            {
+                throw new ArgumentNullException(nameof(mapping));
+            }
+
+            if (!this.mappings.ContainsKey(typeof(TApiEvent)))
+            {
+                throw new InvalidOperationException(
+                    $"API event type {typeof(TApiEvent).Name} mapping is not registered");
+            }
+
+            this.mappings[typeof(TApiEvent)] = (c, e) => mapping(c, e as TApiEvent);
+        }
+
+        public void RemoveMapping<TApiEvent>()
+        {
+            if (!this.mappings.ContainsKey(typeof(TApiEvent)))
+            {
+                throw new InvalidOperationException(
+                    $"API event type {typeof(TApiEvent).Name} mapping is not registered");
+            }
+
+            this.mappings.Remove(typeof(TApiEvent));
         }
     }
 }
