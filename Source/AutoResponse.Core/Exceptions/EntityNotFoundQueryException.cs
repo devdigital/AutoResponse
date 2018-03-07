97d8b92209f7c0f2e998568fc1cc3eb63a2a33ca
devdigital <devdigital.cloud@gmail.com>
2018-03-07 18:24:12 +0000

    chore: include entity not found query parameters

--- Source/AutoResponse.Core/Exceptions/EntityNotFoundQueryException.cs
+++ Source/AutoResponse.Core/Exceptions/EntityNotFoundQueryException.cs
@@ -1,4 +1,6 @@
-﻿namespace AutoResponse.Core.Exceptions
+﻿using System.Text;
+
+namespace AutoResponse.Core.Exceptions
 {
     using System.Collections.Generic;
 
@@ -8,13 +10,29 @@
     public class EntityNotFoundQueryException : AutoResponseException<EntityNotFoundQueryApiEvent>
     {
         public EntityNotFoundQueryException(string code, string entityType, IEnumerable<QueryParameter> parameters)
-            : base($"The {entityType} entity with the provided parameters was not found.", new EntityNotFoundQueryApiEvent(code, entityType, parameters))
+            : base(ToMessage(entityType: entityType, parameters: parameters), new EntityNotFoundQueryApiEvent(code, entityType, parameters))
         {            
         }
 
         public EntityNotFoundQueryException(string entityType, IEnumerable<QueryParameter> parameters)
-            : base($"The {entityType} entity with the provided parameters was not found.", new EntityNotFoundQueryApiEvent(entityType, parameters))
+            : base(ToMessage(entityType: entityType, parameters: parameters), new EntityNotFoundQueryApiEvent(entityType, parameters))
         {            
-        }        
+        }
+
+        private static string ToMessage(string entityType, IEnumerable<QueryParameter> parameters)
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"The {entityType} entity with the provided parameters was not found:");
+
+            foreach (var parameter in parameters)
+            {
+                stringBuilder.AppendLine(string.IsNullOrWhiteSpace(parameter.Value)
+                    ? $"{parameter.Key}"
+                    : $"{parameter.Key}: {parameter.Value}");
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }
