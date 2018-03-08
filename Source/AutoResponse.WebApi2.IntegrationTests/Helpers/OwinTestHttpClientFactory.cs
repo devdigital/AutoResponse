3a6ddcfe4a06c1fbdef63117c37133960ea4559d
devdigital <devdigital.cloud@gmail.com>
2018-03-08 10:17:19 +0000

    chore: add flurl tests

---
+++ Source/AutoResponse.WebApi2.IntegrationTests/Helpers/OwinTestHttpClientFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using Flurl.Http.Configuration;
+using Microsoft.Owin.Testing;
+
+namespace AutoResponse.WebApi2.IntegrationTests.Helpers
+{
+    // Adapted from https://stackoverflow.com/a/33128757/248164
+    public class OwinTestHttpClientFactory : DefaultHttpClientFactory
+    {
+        private readonly TestServer testServer;
+
+        public OwinTestHttpClientFactory(TestServer server)
+        {
+            this.testServer = server;
+        }
+
+        public override HttpMessageHandler CreateMessageHandler()
+        {
+            return this.testServer.Handler;
+        }
+    }
+}
---
+++ Source/AutoResponse.WebApi2.IntegrationTests/Tests/FlurlTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using AutoResponse.Client;
+using AutoResponse.WebApi2.IntegrationTests.Helpers;
+using Flurl.Http;
+using Flurl.Http.Testing;
+using Xunit;
+
+namespace AutoResponse.WebApi2.IntegrationTests.Tests
+{
+    // ReSharper disable StyleCop.SA1600
+    #pragma warning disable SA1600
+    #pragma warning disable 1591
+
+    public class FlurlTests
+    {
+        [Theory]
+        [AutoData]
+        public async Task GetNotFoundHandlesError(string body)
+        {
+            using (var httpTest = new HttpTest())
+            {
+                httpTest.RespondWith(body: body, status: 404);
+
+                try
+                {
+                    var response = await "http://localhost".GetJsonAsync();
+                }
+                catch (FlurlHttpException exception)
+                {
+                    var response = exception.Call?.Response;
+                    if (response == null)
+                    {
+                        throw;
+                    }
+
+                    await response.HandleErrors(
+                        throwOnUnhandledResponses: false);
+                }
+            }
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task GetNotFoundTestServerHandlesError(SampleServerFactory serverFactory)
+        {
+            using (var server = serverFactory.Create())
+            {
+                using (var flurlClient = new FlurlClient())
+                {
+                    flurlClient.Settings.HttpClientFactory =
+                        new OwinTestHttpClientFactory(server);
+
+                    flurlClient.BaseUrl = "http://localhost";
+
+                    try
+                    {
+                        var json = await flurlClient.Request("foo").GetJsonAsync();
+                    }
+                    catch (FlurlHttpException exception)
+                    {
+                        var response = exception.Call?.Response;
+                        if (response == null)
+                        {
+                            throw;
+                        }
+
+                        await response.HandleErrors(
+                            throwOnUnhandledResponses: false);
+                    }
+                }                
+            }
+        }
+
+    }
+}
